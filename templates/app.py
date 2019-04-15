41b3e0a696690267d5f30277b2a438d2ff14b4eb
SahiltheGreat <smbstar1@gmail.com>
2019-04-16 00:50:47 +0530

    Github commits

--- templates/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask,render_template
-from flask_sqlalchemy import SQLAlchemy
-from flask_wtf import FlaskForm
-from wtforms_sqlalchemy.fields import QuerySelectField
-
-app = Flask(__name__)
-
-app,config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////mn
-
